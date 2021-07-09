aedd5eb511d630720a45e789e9792e0ab7731f83
Kieran <unconfigured@null.spigotmc.org>
2021-07-09 21:21:55 +0200

    small fixs

--- JS/parallax.js
+++ JS/parallax.js
@@ -44,28 +44,29 @@ function parallax(e){
  */
 function floating(){
 
-    if(isFloating){
-        let seconds = new Date().getTime() / 1000;
-        let angle = (seconds * 40) % 360;
+    let seconds = new Date().getTime() / 1000;
+    let angle = (seconds * 40) % 360;
 
-        let width2 = window.innerWidth / 2;
-        let height2 = window.innerHeight / 2;
+    let width2 = window.innerWidth / 2;
+    let height2 = window.innerHeight / 2;
 
-        let x = width2 + Math.cos(angle/180*Math.PI) * width2;
-        let y = height2 + Math.sin(angle/180*Math.PI) * height2;
+    let x = width2 + Math.cos(angle/180*Math.PI) * width2;
+    let y = height2 + Math.sin(angle/180*Math.PI) * height2;
 
-        let parallaxElements = document.querySelectorAll("[data-parallax]");
-        parallaxElements.forEach(element => {
+    let parallaxElements = document.querySelectorAll("[data-parallax]");
+    parallaxElements.forEach(element => {
 
-            //1 speed means move by 1% of main element
-            let speed = element.getAttribute("data-parallax");
-            
-            let moveX = - x * speed / 100; 
-            let moveY = - y * speed / 100;
+        //1 speed means move by 1% of main element
+        let speed = element.getAttribute("data-parallax");
+        
+        let moveX = - x * speed / 100; 
+        let moveY = - y * speed / 100;
 
-            element.style.transform = `translateX(${moveX}px) translateY(${moveY}px)`;
+        element.style.transform = `translateX(${moveX}px) translateY(${moveY}px)`;
 
-        });
-    }
+    });
+
 }
-setInterval(floating, 100);
+setInterval(() => {
+    if(isFloating) requestAnimationFrame(floating);
+}, 100);
